7fee8537b904349b38c100d2a5864a301474391d
Milad Moafi <miladmoafi@outlook.com>
2019-05-08 21:38:47 +0430

    antd added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { Button } from "antd";
+import "antd/dist/antd.css";
+import { Button, DatePicker } from "antd";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Button />
+        <DatePicker />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
